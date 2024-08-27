8da12836abc72621eab68fd24324734d1f70590c
Azhwake <azhwaq2002@gmail.com>
2024-08-27 15:40:00 +0300

    DONE

--- challenge.js
+++ challenge.js
@@ -1,2 +1,7 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculatePoints(Wins, draws,losses) {
+    let totalPoints = (Wins * 3) +  (draws * 2) + (losses * 0);
+    console.log(totalPoints)
+}
+calculatePoints(5,2,3); 
